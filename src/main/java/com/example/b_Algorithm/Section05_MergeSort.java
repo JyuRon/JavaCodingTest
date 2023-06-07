dc696b09c4804c1215fdc0e5596d5a99b8c4ca8b
Jyuka <yoyksks3@gmail.com>
2023-06-07 18:57:41 +0900

    Merge Sort (진행중)

---
+++ src/main/java/com/example/b_Algorithm/Section05_MergeSort.java
@@ -0,0 +1,47 @@
+package com.example.b_Algorithm;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * 재귀용법을 활용한 정렬 알고리즘
+ * 리스트를 절반으로 잘라 비슷한 크기의 두 부분 리스트로 나눈다. --> 크기가 1개가 될때까지 끝까지 나눔
+ * 각 부분 리스트를 재귀적으로 합병 정렬을 이용해 정렬한다.
+ * 두 부분 리스트를 다시 하나의 정렬된 리스트로 합병한다.
+ */
+public class Section05_MergeSort {
+
+    public List<Integer> merge(List<Integer> leftList, List<Integer> rightList){
+
+    }
+
+    public List<Integer> mergeSplitFunc(List<Integer> dataList){
+        if(dataList.size() <= 1){
+            return dataList;
+        }
+
+        int medium = dataList.size() / 2;
+        List<Integer> leftList = dataList.subList(0, medium);
+        List<Integer> rightList = dataList.subList(medium, dataList.size());
+
+        return merge(mergeSplitFunc(leftList), mergeSplitFunc(rightList));
+    }
+
+    public List<Integer> sort(List<Integer> dataList){
+
+
+        return dataList;
+    }
+
+    public static void main(String[] args) {
+        Section05_MergeSort mergeSort = new Section05_MergeSort();
+        List<Integer> list = new ArrayList<>();
+        for (int i = 0; i < 20; i++) {
+            list.add((int)(Math.random()*100));
+        }
+        System.out.println(list);
+        System.out.println(mergeSort.sort(list));
+    }
+
+
+}
