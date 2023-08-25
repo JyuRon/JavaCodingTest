ed115e7892f13da9765ae50569ce028ec6223113
Jyuka <yoyksks3@gmail.com>
2023-08-25 20:45:27 +0900

    SQL : rank 1문제(row_number)

--- sql/06_rank.sql
+++ sql/06_rank.sql
@@ -18,3 +18,17 @@ from tproduction
 group by enumber
 limit 10;
 ;
+
+/**
+  14
+  현재 판매하는 제품들 중 장농의 인기가 많아져 판매 가능한 장농의 재고를 확인하기 위하여
+  장농 생산이력을 전부 출력하되 생산량이 높은순서대로 생산한 직원의 코드와 제품코드, 생산량을 순위를 매겨 출력하시오.
+  (공동 순위가 나오지 않게 출력 해야 하며 또한 동률일경우 직원코드(ENumber)가 작은 코드가 우선순위를 가지 도록 한다, 장농의 제품코드는 I2003번이다)
+ */
+
+ select
+     enumber,pnumber ,pcount,
+     row_number() over (partition by inumber order by pcount desc, enumber) as rank
+ from tproduction
+ where inumber = 'I2003'
+ ;
